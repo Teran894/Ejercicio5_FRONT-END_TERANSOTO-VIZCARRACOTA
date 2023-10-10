ed06683f8e4b70b07a0d27db25cf878ff3e3e926
Jose Luis Teran Soto <77344824+Teran894@users.noreply.github.com>
2023-10-09 20:30:22 -0700

    Commit base del ejercicio con aspecto visual
    Contiene la base del ejercicio de front-end, el aspecto visual, la tabla, la funcionalidad basica

---
+++ README.md
@@ -0,0 +1,51 @@
+# FrontEnd-Ejercicio-5
+Ejercicio 5 para Front End. 
+
+Ejercicio realizado por estudiantes:
+  - Jose Luis Teran Soto
+  - Alfredo Vizcarra Cota
+  
+ 
+  ## Estructura del proyecto
+
+Estructura inicial del proyecto basada en componentes:
+
+```bash
+
+Ejercicio-5/              # Carpeta principal del proyecto
+│
+├── src
+│   ├── assets/           # Carpeta que almacena archivos estáticos.
+│   │    ├── css/         # Carpeta para archivos CSS compilados
+│   │    └── img/  		  # Carpeta para images estaticas
+│	│		  ├──catalogo_IMG 
+│	│		  ├──nuevas ventas_IMG
+│	│		  └──ventas_IMG
+│   │
+│   │
+│   ├── catalogo/         # Carpeta para el catalogo de modelos
+│   │    ├── index.html   # Página HTML de modelos
+│   │    ├── script.js    # JavaScript específico de modelos
+│   │    └── styles.css   # Estilos CSS específicos de modelos
+│   │
+│   │
+│   ├── ventas/           # Recursos relacionados con la API (Tmp).
+│   │    ├── index.html   # Página HTML de ventas
+│   │    ├── script.js    # JavaScript específico de ventas
+│   │    └── styles.css   # Estilos CSS específicos de ventas
+│   │
+│   │
+│   ├── nuevas ventas/    # Recursos relacionados con la API (Tmp).
+│   │    ├── index.html   # Página HTML de ventas
+│   │    ├── script.js    # JavaScript específico de ventas
+│   │    └── styles.css   # Estilos CSS específicos de ventas
+│	│
+│	│
+│   └─── index.html       # Archivo HTML principal con home
+│
+│
+├── README.md             # Documentación del proyecto
+│
+├── package.json
+
+```
---
+++ src/catalogo/script.js
@@ -0,0 +1,102 @@
+//#region MODELO DE DATOS
+class CatalogoJuegos{
+
+constructor (id, title, platform, description, price, rating, image){
+    this.id = id;
+    this.title =title;
+    this.platform = platform;
+    this.description = description;
+    this.price = price;
+    this.rating = rating;
+    this.image = image ;
+    }
+}
+
+const game1 = new CatalogoJuegos(1,"Honkai Star Rail","PC|PS","Gacha turn based game",0,7.8,"honkaiST.jpg")
+const game2 = new CatalogoJuegos(2,"Starfield","Xbox|PC","Open world space exploration game",60,8.6,"starfield.jpg")
+const game3 = new CatalogoJuegos(3,"Hollow Knight","Xbox|PC|Switch","Slash and dash bug game",45,9.2,"hollowknight.jpg")
+
+const CatalogoJuegosList = [game1,game2,game3];
+
+console.log('Impresion en consola de elementos accesados con forEach(): ');
+CatalogoJuegosList.forEach(item => {console.log(item)});
+//#endregion
+
+//#region VISTA HTML
+function displayTable(games){
+    clearTable();
+
+    showLoadingMessage();
+
+    setTimeout(() => {
+
+        if (games.length === 0) {
+    
+          showNotFoundMessage();
+    
+        } else {
+    
+            hideMessage();
+    
+            const tablaBody = document.getElementById('data-table-body');
+    
+            const imagePath = `../assets/img/catalogo_IMG/`;
+    
+            games.forEach(game => {
+    
+              const row = document.createElement('tr');
+    
+              row.innerHTML = `
+                <td> ${game.id} </td>
+                <td> <img src="${imagePath + game.image}" alt="${game.title}" width="100"> </td>
+                <td>${game.title}</td>
+                <td>${game.platform}</td>
+                <td>${game.description}</td>
+                <td>${game.price}</td>
+                <td>${game.rating}</td>
+              `;
+    
+              tablaBody.appendChild(row);
+    
+            });
+    
+        }
+    
+      }, 2000);
+
+}
+
+function clearTable() {
+    const tableBody = document.getElementById('data-table-body');
+  
+    tableBody.innerHTML = '';
+  }
+  
+  
+  function showLoadingMessage() {
+    const messageNotFound = document.getElementById('message-not-found');
+  
+    messageNotFound.innerHTML = 'Cargando...';
+  
+    messageNotFound.style.display = 'block';
+  }
+  
+  
+  function showNotFoundMessage() {
+    const messageNotFound = document.getElementById('message-not-found');
+  
+    messageNotFound.innerHTML = 'No se encontraron juegos con el filtro.';
+  
+    messageNotFound.style.display = 'block';
+  }
+  
+  
+  function hideMessage() {
+    const messageNotFound = document.getElementById('message-not-found');
+  
+    messageNotFound.style.display = 'none';
+  }
+
+//#endregion
+
+displayTable(CatalogoJuegosList);
