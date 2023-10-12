c3f246e00e5e0cd0557eb3230e32fd2c3411c3de
Jose Luis Teran <superviper.jlts@gmail.com>
2023-10-11 20:04:22 -0700

    Refinamiento de aspecto visual
    Corregir errores de aspectos visuales y uno que otro error de CSS

---
+++ src/script.js
@@ -0,0 +1,14 @@
+// Recuperar datos del formulario
+const contactoForm = document.getElementById('contacto-form');
+
+contactoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  alert('Gracias por contactarnos - ' + nombre + ' | ' + email + ' | ' + mensaje);
+
+  contactoForm.reset();
+});
